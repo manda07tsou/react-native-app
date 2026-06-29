104040733eadf02fd3bf16693da7e82c6b57aad9
vonintsoa <rhjmanda@gmail.com>
2026-06-29 16:56:38 +0300

    feat: demande permission

--- src/app/contact/index.tsx
+++ src/app/contact/index.tsx
@@ -1,7 +1,20 @@
 import AppText from "@/components/appText";
 import { RootView } from "@/components/rootView";
+import {requestPermissionsAsync, Contact, Fields} from "expo-contacts"
+import { useEffect } from "react";
+
 
 export default function Index(){
+    useEffect(() => {
+        (async () => {
+            const { status } = await requestPermissionsAsync()
+
+            if(status == "granted"){
+                console.log(await Contact.getAll())
+            }
+        })()
+    }, [])
+
     return <RootView>
         <AppText>Index tabs</AppText>
     </RootView>
